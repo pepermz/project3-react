86e07ccff09e840258fafaea86b8fd693a71dcc5
pepermz <jose.ram.18@gmail.com>
2022-05-18 10:33:35 -0500

    added card

--- src/components/SearchedItem.js
+++ src/components/SearchedItem.js
@@ -1,9 +1,14 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect} from "react"
 import Banner from "./Banner";
+import airbnb from "../assets/airbnb.png"
+import foursquare from "../assets/foursquarelogo.png"
+import google from "../assets/googlelogo.png"
+import kayak from "../assets/kayak.png"
 
 const SearchedItem = (props) => {
     const [image, setLocation] = useState({})
+    const apikey = process.env.ACCESS_KEY;
     const  auth_key = btoa('4f96b20b94d0c2e382d8d516663744f0:3418c931bd19226ace3c0dea40b7b54c').toString('base64');
     const { id }= useParams();
     const URL = `https://api.roadgoat.com/api/v2/destinations/${id}`
@@ -28,8 +33,23 @@ const SearchedItem = (props) => {
           <div>
             <Banner />
            <h1>{image.attributes && image.attributes.name}</h1>
-           <a href={image.attributes && image.attributes.airbnb_url}>Click here for Airbnb</a>
-           {/* <div>{image.attributes && image.relationships.photos.data[0].id} </div> */}
+           <p>Population: {image.attributes && image.attributes.population}</p>
+           <div className="grid">
+           <a className="card1"  href={image.attributes && image.attributes.airbnb_url}>
+           <img className="card-img"  alt="airbnb"src={airbnb}/>
+           </a>
+
+           <a className="card1" href={image.attributes && image.attributes.google_events_url}>
+           <img className="card-img"  alt="google"src={google}/>
+           </a>
+           <a className="card1" href={image.attributes && image.attributes.kayak_car_rental_url}>
+           <img className="card-img"  alt="kayak"src={kayak}/>
+           </a>
+           <a className="card1" href={image.attributes && image.attributes.foursquare_url}>
+           <img className="card-img"  alt="foursquare"src={foursquare}/>
+           </a>
+           </div>
+           {/* <p>Budget: {image.attributes && image.attributes.budget[0]}</p> */}
            {/* <div> {image.relationships.photos.data.id ? <img src={(image.relationships.photos.data.id)} alt={`name`} /> : null } </div> */}
           </div>
         )
