52e5c31ac5496a8155242166ce7d2c03a0afdde4
pepermz <jose.ram.18@gmail.com>
2022-05-19 09:24:16 -0500

    added watermark to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,11 @@ import FooterImg from "../assets/GitHub-Mark-Light-32px.png"
 function Footer() {
   return (
     <div>
-    <div className='footer1'>Developed By Jose Ramirez</div>
-    <a className="github" href=''>
+    <div className='footer1'>Developed By Jose Ramirez 
+    <a className="github" href='https://github.com/pepermz'>
         <img className="github1"  alt="github"src={FooterImg}/>
     </a>
+    </div>
     </div>
   )
 }
